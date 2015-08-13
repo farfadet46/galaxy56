87f285244d82605000feeab86cc6a6be5dfa861a
farfadet46 <farfadet46@gmail.com>
2015-08-13 18:57:29 +0200

    Finalisation du mod 'casque'

--- mods/casque/init.lua
+++ mods/casque/init.lua
@@ -1,50 +1,63 @@
 -- *** CC0 by @farfadet46 *** --
+-- ok le 13 août 2015
 
---[[
+casque = {}
+local casque_actif = true
+
 minetest.register_on_joinplayer(function(player)
-	local idx = player:hud_add({
-		hud_elem_type = "image",		
-		scale = {x=-100 , y=-100},
+	porte_casque(player)
+end)
+
+minetest.register_chatcommand("casque",{
+	desciption = "porter / enlever le casque",
+		func = function( name , param)
+		local player = minetest.get_player_by_name(name)
+		
+-- Si le joueur veut porter son casque
+			if param == "on" then 
+				if casque_actif == true then
+					minetest.chat_send_player(name,"Vous portez deja votre casque")
+					return
+				elseif casque_actif == false then
+					minetest.chat_send_player(name,"Vous avez mis votre casque")
+					
+					porte_casque(player)
+					
+					casque_actif = true
+					return
+				end
+				
+-- Sinon si le joueur veut enlever son casque
+			elseif param == "off" then
+				if casque_actif == true then --on porte le casque
+					minetest.chat_send_player(name,"Vous avez enlevé votre casque")
+					player:hud_remove(casque[player:get_player_name()])
+					casque_actif = false
+					return
+				elseif casque_actif == false then --on ne porte pas le casque
+					minetest.chat_send_player(name,"Vous ne portez pas encore votre casque")
+					return
+				end
+				
+-- Sinon si erreur dans la commande
+			else
+				minetest.chat_send_player(name,"Veauillez utiliser la commande '/casque' suivie de 'on' ou 'off' pour porter/enlever votre casque.")
+				return
+			end
+		
+		return true
+	end,
+	})
+	
+function porte_casque(player)
+	casque[player:get_player_name()] = player:hud_add({
+		hud_elem_type = "image",
+		name="casque",
+		scale = {x=1 , y=1},
 		alignment = { x=0, y=0 },
 		offset = { x=0, y=0 },
 		position =  { x=0.5, y=0.5 },
 		text = "casque.png" -- text = image a placer
 		})
-end)
-]]--
-local posit = nil
-
-minetest.register_chatcommand("casque",{
-	params = "<on_off>",
-	desciption = "porter / enlever le casque",
-	--privs = {talk = true},
-		func = function( player , on_off)
-			posit = player:getpos()
-			minetest.chat_send_all(posit)
-			if on_off == "on" then
-				--[[	idx = player:hud_add({
-					hud_elem_type = "image",		
-					scale = {x=-100 , y=-100},
-					alignment = { x=0, y=0 },
-					offset = { x=0, y=0 },
-					position =  { x=0.5, y=0.5 },
-					text = "casque.png" -- text = image a placer
-				}) 
-				]]--
-				minetest.chat_send_all("casque ON ")
-			elseif on_off == "off" then
-			--	player:hud_remove(idx)
-				minetest.chat_send_all("casque OFF ")
-			else
-				minetest.chat_send_all("Erreur dans la commande casque. seule les valeurs 'on' et 'off' sont autorisee.")
-			end
-		
-		return true
-		--[[
-		func = function( _ , port)
-		minetest.chat_send_all("le joueur a dit : " .. port)
-		return true, "Text was sent successfully"
-		]]--
-	end,
-	})
+end
 	
