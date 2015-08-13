96781d65257ac0684aee5d54508f7380a9afc6bc
farfadet46 <farfadet46@gmail.com>
2015-08-13 13:36:30 +0200

    test

--- mods/galaxy_spikes/init.lua
+++ mods/galaxy_spikes/init.lua
@@ -15,38 +15,27 @@ galaxy_spikes.register_spike = function(color)
 	
 	minetest.register_decoration({
 		deco_type = "simple",
-		place_on = {"group:stone","group:dirt","group:sand","default:dirt_with_grass"},
+		place_on = {"group:stone","group:dirt","default:dirt_with_grass"},
 		sidelen = 8,
 		y_min = -31000,
 		y_max = 31000,
 		--fill_ratio = 0.02,
 		decoration = "galaxy_spikes:spike_"..color,
 	})
-	--[[
 	minetest.register_decoration({
 		deco_type = "simple",
 		place_on = {"group:sand"},
 		sidelen = 80,
 		y_min = -31000,
 		y_max = 31000,
-		decoration = "galaxy_spikes:spike_"..color,
-	})
-
-	minetest.register_decoration({
-		deco_type = "simple",
-		place_on = {"group:stone"},
-		sidelen = 80,
-		y_min = -31000,
-		y_max = 31000,
-		decoration = "galaxy_spikes:spike_"..color,
-	})
-	]]--
+		decoration = "galaxy_spikes:spike_yellow",
+	})	
 end
 
 galaxy_spikes.register_spike("red")
-galaxy_spikes.register_spike("green")
-galaxy_spikes.register_spike("blue")
+--galaxy_spikes.register_spike("green")
+--galaxy_spikes.register_spike("blue")
 galaxy_spikes.register_spike("olive")
 galaxy_spikes.register_spike("yellow")
 galaxy_spikes.register_spike("black")
-galaxy_spikes.register_spike("purple")
+--galaxy_spikes.register_spike("purple")
