63ad05c64b5711c666d7edf23499ff62e99ead77
farfadet46 <farfadet46@gmail.com>
2015-08-15 18:07:31 +0200

    bug mineurs

--- mods/laser/init.lua
+++ mods/laser/init.lua
@@ -6,19 +6,20 @@ minetest.register_tool("laser:epee_sword", {
 	
 	--old_on_use = on_use or function() end, 
 		
-	--on_punch = function(itemstack, user, pointed_thing) 
+	on_punch = function(itemstack, user, pointed_thing) 
 	--	old_on_use(itemstack, user, pointed_thing) 
-		minetest.sound_play("laser_sound", {pos = pointed_thing, gain = 0.5,}),
+		minetest.sound_play("laser_sound", {pos = pointed_thing, gain = 0.5,})
+		
 		--return
-	--end, 
+	end, 
 	
 	tool_capabilities = {
 		full_punch_interval = 0.1,
 		max_drop_level=3,
 		groupcaps={
-			cracky={times={[1]=1.0, [2]=0.5, [3]=0.5}, uses=30, maxlevel=3},
-			crumbly={times={[1]=1.0, [2]=0.5, [3]=0.5}, uses=30, maxlevel=3},
-			snappy={times={[1]=1.0, [2]=0.5, [3]=0.5}, uses=30, maxlevel=3}
+			cracky={times={[1]=0.1, [2]=0.1, [3]=0.1}, uses=300, maxlevel=3},
+			crumbly={times={[1]=0.1, [2]=0.1, [3]=0.1}, uses=300, maxlevel=3},
+			snappy={times={[1]=0.1, [2]=0.1, [3]=0.1}, uses=300, maxlevel=3}
 		}
 	}
 })
