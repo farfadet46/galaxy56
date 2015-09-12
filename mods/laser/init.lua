f19249b78591370cebfb5ab39575b69d49a8a7f8
farfadet46 <farfadet46@gmail.com>
2015-09-12 13:40:53 +0200

    Update init.lua

--- mods/laser/init.lua
+++ mods/laser/init.lua
@@ -19,24 +19,25 @@ minetest.register_tool("laser:epee_sword", {
 		end,
 
 	on_use = function(itemstack, user, pointed_thing)
-		if pointed_thing ~= nil then
-			local pos = minetest.get_pointed_thing_position(pointed_thing)
-			local node = minetest.get_node(pos)
+		local pos = minetest.get_pointed_thing_position(pointed_thing)
+		if pos == nil then
+			return
+		else
+		local node = minetest.get_node(pos)
 			if node.name then
-				-- local name = node.name:gsub("^.+:", "micronode:")
-				-- if minetest.registered_items[name] then
-				--	local stack = {name=name, count=MICRONODE_STACK_MAX}
-					local inv = user:get_inventory()
-					if inv then
-						inv:add_item("main", node.name)
-					end
-					minetest.remove_node(pos)
-					minetest.sound_play("laser")
-			--	end
+			-- local name = node.name:gsub("^.+:", "micronode:")
+			-- if minetest.registered_items[name] then
+			--	local stack = {name=name, count=MICRONODE_STACK_MAX}
+				local inv = user:get_inventory()
+				if inv then
+					inv:add_item("main", node.name)
+				end
+				minetest.remove_node(pos)
+				minetest.sound_play("laser")
 			end
-			--itemstack:add_wear(MICRONODE_LASER_WEAR)
-			return itemstack
 		end
+		--itemstack:add_wear(MICRONODE_LASER_WEAR)
+		return itemstack
 	end,
 	
 	allow_metadata_inventory_take = allow_metadata_inventory_take,
